203823b0d603deacefa990d6af5d88302543a680
M.Etka Uzun <m.etkauzun@MEtka-MacBook-Pro.local>
2020-04-03 10:39:32 +0300

    firsr commit

---
+++ firsDay.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+int main() 
+{
+    
+    vector<int> g1;
+    vector< vector<int> > g2;
+    g1.push_back(5);
+    for(int i = 0; i < g1.size(); i++){
+        printf("%d" ,g1[i]);
+        cout << g1[i];
+    }
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 6; j++){
+            g2[i].push_back(j);
+        }
+    }
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 6; j++){
+            cout << g2[i][j] << " ";
+        }
+    }
+    cout << "Hello, World!";
+    return 0;
+}
